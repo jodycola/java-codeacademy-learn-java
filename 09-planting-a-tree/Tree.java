49e6c7ec95269980ee0477eaff42317aa9ad1bbe
Kody Samaroo <kody.samaroo@gmail.com>
2023-05-05 16:23:32 -0400

    planting a tree completed

---
+++ 09-planting-a-tree/Tree.java
@@ -0,0 +1,14 @@
+// Define your tree class in this file!
+public class Tree {
+    public static void main(String[] args){
+      // We’re going to introduce ourselves and print a tree to the screen.
+      System.out.println("Hey there, I'm Jody and I'm learning to code in Java!");
+      System.out.println("Ready to get my hands dirty!");
+      System.out.println("* ** *");
+      System.out.println("******");
+      System.out.println(" **** ");
+      System.out.println("  **  ");
+      System.out.println("  **  ");
+  
+    }
+  }
