c1890a058e6c903d7b75af38d1be0229ef1069ea
Kody Samaroo <kody.samaroo@gmail.com>
2023-05-18 22:24:20 -0400

    adding an item

---
+++ 09-learn-java-arraylists/03-adding-an-item/ToDos.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+class ToDos {
+    
+  public static void main(String[] args) {
+    
+    ArrayList<String> toDoList = new ArrayList<String>();
+    String toDo1 = "Water plants";
+    String toDo2 = "Wash dishes";
+    String toDo3 = "Take out trash";
+
+    // Add more to-dos here:
+    toDoList.add(toDo1);
+    toDoList.add(toDo2);
+    toDoList.add(toDo3);
+
+    
+    // Add to-dos to toDoList
+    
+    
+    System.out.println(toDoList);
+      
+    
+  }
+  
+}
